e8efbe22dcd9ca440b26d1896557ddfbc4ecc3ff
Wlad555 <102764055+Wlad555@users.noreply.github.com>
2023-02-07 20:18:13 +0100

    Änderung

--- src/TikTakTo.java
+++ src/TikTakTo.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 public class TikTakTo {
+    static char[] spielfeld = new char[9];
+
     public static void spielFeld() {
-        char[] spielfeld = new char[9];
-        for (int i = 0; i <= spielfeld.length; i++){
-            Scanner eingabe = new Scanner(System.in);
-            int stelle = eingabe.nextInt();
-            char symbol = eingabe.next().charAt(0);
-            spielfeld[stelle - 1] = symbol;
             System.out.println("   " + spielfeld[0] + "  |  " + spielfeld[1] + "  |  " + spielfeld[2] + "\n-------------------");
             System.out.println("   " + spielfeld[3] + "  |  " + spielfeld[4] + "  |  " + spielfeld[5] + "\n-------------------");
             System.out.println("   " + spielfeld[6] + "  |  " + spielfeld[7] + "  |  " + spielfeld[8]);
-        }
-        // Eine if die es nicht erlaubt befüllte stellen zu befüllen und nicht aus dem werte bereich raus und das nur das symbol x und y genommen werden kann
-
-        /*System.out.println("   " + spielfeld[0] + "  |  " + spielfeld[1] + "  |  " + spielfeld[2] + "\n-------------------");
-        System.out.println("   " + spielfeld[3] + "  |  " + spielfeld[4] + "  |  " + spielfeld[5] + "\n-------------------");
-        System.out.println("   " + spielfeld[6] + "  |  " + spielfeld[7] + "  |  " + spielfeld[8]);*/
     }
     public static void gewinnerTest(){
-
+        if(spielfeld[0]=='X' && spielfeld[1]=='X' && spielfeld[2]=='X' || spielfeld[3]=='X' && spielfeld[4]=='X' && spielfeld[5]=='X' || spielfeld[6]=='X' && spielfeld[7]=='X' && spielfeld[8]=='X' || spielfeld[0]=='X' && spielfeld[4]=='X' && spielfeld[8]=='X' || spielfeld[2]=='X' && spielfeld[4]=='X' && spielfeld[6]=='X' || spielfeld[0]=='X' && spielfeld[3]=='X' && spielfeld[6]=='X' || spielfeld[1]=='X' && spielfeld[4]=='X' && spielfeld[7]=='X' || spielfeld[2]=='X' && spielfeld[5]=='X' && spielfeld[8]=='X'){
+            System.out.println("SPIELER 1 SIEGT!!!");
+        }
+        if(spielfeld[0]=='O' && spielfeld[1]=='O' && spielfeld[2]=='O' || spielfeld[3]=='O' && spielfeld[4]=='O' && spielfeld[5]=='O' || spielfeld[6]=='O' && spielfeld[7]=='O' && spielfeld[8]=='O' || spielfeld[0]=='O' && spielfeld[4]=='O' && spielfeld[8]=='O' || spielfeld[2]=='O' && spielfeld[4]=='O' && spielfeld[6]=='O'|| spielfeld[0]=='O' && spielfeld[3]=='O' && spielfeld[6]=='O' || spielfeld[1]=='O' && spielfeld[4]=='O' && spielfeld[7]=='O' || spielfeld[2]=='O' && spielfeld[5]=='O' && spielfeld[8]=='O'){
+            System.out.println("SPIELER 2 SIEGT!!!");
+        }
     }
     public static void main(String[] args) {
-        spielFeld();
-    }
+        for (int i = 0; i <= spielfeld.length; i++){
+            Scanner sc = new Scanner(System.in);
+            int stelle = sc.nextInt();
+            char symbol = sc.next().charAt(0);
+            spielfeld[stelle - 1] = symbol;
+            spielFeld();
+            gewinnerTest();
 
+        }
+    }
 }
